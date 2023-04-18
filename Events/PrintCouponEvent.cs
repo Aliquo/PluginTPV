cc1e2f41ba5a7e17d488154eee9a17ed4c47e6b6
Víctor González <victor.gonzalez@abacois.com>
2023-04-18 08:53:07 +0200

    Add project files.

---
+++ Events/PrintCouponEvent.cs
@@ -0,0 +1,81 @@
+﻿using AliquoTPV.Extensibility;
+using System;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PluginTPV_Demo.Events
+{
+
+    [Export(typeof(AliquoTPV.Extensibility.Events))]
+    [EventsMetadata()]
+    public class PrintCouponEvent : AliquoTPV.Extensibility.Events
+    {
+       
+        public PrintCouponEvent()
+        {
+            PrintAfter += Eventos_PrintAfter;
+        }
+
+
+        private void Eventos_PrintAfter(IHost sender, PrintEventArgs e)
+        {
+
+            // code example with functionality to print using Crystal Report for coupon printing
+            try
+            {
+
+                if (e.Type == DocumentType.SalesDocument || e.Type == DocumentType.SalesInvoice)
+                {
+
+                    // the possibility of printing a voucher is assessed if is a ticket or invoice
+                    // and is not a copy, moreover, there must be data
+                    if (!e.IsCopy && e.Data != null && e.Data.Tables.Contains("Notas") && e.Data.Tables["Notas"].Rows.Count > 0)
+                    {
+
+                        var docDate = Convert.ToDateTime(e.Data.Tables["Notas"].Rows[0]["Fecha"]);
+                        var docAmount = Convert.ToDecimal(e.Data.Tables["Notas"].Rows[0]["ImporteTotal"]);
+
+                        // print a coupon for 2% of the sales amount if it exceeds the amount of 30
+                        if (docAmount >= 30m)
+                        {
+                            // we create a schema to pass it to the report
+                            var data = new DataSet();
+
+                            data.Tables.Add(e.Data.Tables["PRM_Entorno"].Copy());
+
+                            data.Tables.Add("Vale");
+                            {
+                                var withBlock = data.Tables["Vale"];
+                                withBlock.Columns.Add("Fecha", typeof(DateTime));
+                                withBlock.Columns.Add("Importe", typeof(decimal));
+
+                                // calculate the amount of the coupon
+                                docAmount = Math.Round(docAmount * 0.02m, 2);
+                                withBlock.Rows.Add(docDate, docAmount);
+                            }
+
+
+                            // ATTENTION: the following line must be commented out because
+                            // only used to generate the schema and use it in the report,
+                            // data.WriteXml(IO.Path.Combine(Application.StartupPath, "Plugin\Resources\coupon_schema.xml"), XmlWriteMode.WriteSchema)
+
+                            // send the printout using the host
+                            sender.ReportPrint(System.IO.Path.Combine(Application.StartupPath, @"Plugin\Resources\coupon.rpt"), e.PrinterName, 1, data);
+                        }
+                    }
+
+                         
+                }
+            }
+
+            catch (Exception ex)
+            {
+                // check the error and display the message
+                sender.ShowError("Plugin (PrintAfter)", ex);
+            }
+
+        }
+
+    }
+}
---
+++ Events/PrintOrderEvent.cs
@@ -0,0 +1,40 @@
+﻿using AliquoTPV.Extensibility;
+using System.ComponentModel.Composition;
+
+namespace PluginTPV_Demo.Events
+{
+
+    [Export(typeof(AliquoTPV.Extensibility.Events))]
+    [EventsMetadata()]
+    public class PrintOrderEvent : AliquoTPV.Extensibility.Events
+    {
+
+        public PrintOrderEvent()
+        {
+            PrintBefore += Eventos_PrintBefore;
+        }
+
+        private void Eventos_PrintBefore(IHost sender, PrintEventArgs e)
+        {
+
+            // example to replace the order printing functionality
+            if (e.Type == DocumentType.SalesOrder)
+            {
+
+                // if data exists then another printing system is used instead of Crystal Report 
+                if (e.Data != null && e.Data.Tables.Contains("Notas") && e.Data.Tables["Notas"].Rows.Count > 0)
+                {
+                    // indicates that the usual printing system is to be replaced
+                    e.Handled = true;
+
+                    using (var printOrder = new Tools.PrintOrder(sender, e.Data.Tables["Notas"].Rows[0]))
+                    {
+                        printOrder.Print(e.PrinterName);
+                    }
+                }
+            }
+            
+        }
+
+    }
+}
