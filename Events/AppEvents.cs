921826b37531c79845dbe9cbfb1f43db95d18382
José Palomo <victor.gonzalez@abacois.com>
2023-07-17 09:10:03 +0200

    Implementation of events AppLoaded and AppClosed

---
+++ Events/AppEvents.cs
@@ -0,0 +1,29 @@
+﻿using AliquoTPV.Core;
+using AliquoTPV.Extensibility;
+using System;
+using System.ComponentModel.Composition;
+
+namespace PluginTPV_Demo.Events
+{
+    [Export(typeof(AliquoTPV.Extensibility.Events))]
+    [EventsMetadata()]
+    internal class AppEvents : AliquoTPV.Extensibility.Events
+    {
+        public AppEvents()
+        {
+            this.AppLoaded += AppEvents_AppLoaded;
+            this.AppClosed += AppEvents_AppClosed;
+        }
+
+        private void AppEvents_AppLoaded(IHost sender, EventArgs e)
+        {
+            Console.WriteLine("Loaded application...");
+        }
+
+        private void AppEvents_AppClosed(IHost sender, EventArgs e)
+        {
+            Console.WriteLine("Closed application...");
+        }
+              
+    }
+}
